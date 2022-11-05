0e8c53b5c761a3d9f4a4bc46c97c62c048c2b3bd
Fedor Safonov <safonovfedya696@gmail.com>
2022-11-05 11:39:32 +0300

    hw done

---
+++ src/models/__init__.py
@@ -0,0 +1,15 @@
+from .model_fit_predict import (
+    train_model,
+    predict_model,
+    evaluate_model,
+    serialize_model,
+    load_model
+)
+
+__all__ = [
+    "train_model",
+    "predict_model",
+    "evaluate_model",
+    "serialize_model",
+    "load_model"
+]
