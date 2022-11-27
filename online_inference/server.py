ded4b63589caf83ec081747de038d06bdfe12096
Fedor Safonov <safonovfedya696@gmail.com>
2022-11-27 15:59:05 +0300

    rest service

---
+++ online_inference/server.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import uvicorn
+from fastapi import FastAPI
+import logging
+from entities.model_funcs import *
+
+app = FastAPI()
+model = None
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+
+@app.on_event("startup") # Инициализируем пайплан, хранящийся на сервере
+def loading_model():
+    global model
+    model_path = os.getenv("PATH_TO_MODEL")
+    if model_path is None:
+        err = "PATH_TO_MODEL was not specified"
+        logger.error(err)
+        raise RuntimeError(err)
+    model = load_model(model_path)
+    logger.info(f"Loaded model from {model_path}")
+
+
+@app.get("/")
+def root():
+    return "Model online inference"
+
+
+@app.get("/model")
+def check_model():
+    return "[ERROR]: Model not initialized" if model is None else "[OK]: Model initialized"
+
+@app.get("/predict/", response_model=List[PredictResponse])
+def predict(request: InputDataRequest):
+    return make_predict(request, model)
+
+
+FEATURES = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg",
+            "thalach", "exang", "oldpeak", "slope", "ca", "thal", "id"]
+
+DATA = [[69.0, 1.0, 0.0, 140.0, 239.0, 1.0, 2.0,
+        151.0, 0.0, 1.8, 2.0, 0.0, 1.0, 6.0],
+       [71.0, 0.5, 0.0, 145.0, 200.0, 1.0, 2.0,
+        151.0, 0.0, 1.8, 2.0, 0.0, 1.0, 8.0]]
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5050)
